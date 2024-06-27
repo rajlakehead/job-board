0389b0701ef3b31ecc4cd8936051b5dda9fbe98d
Raj Patel <patelraj24365@gmail.com>
2024-06-27 15:47:48 -0400

    create companies

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,4 +4,14 @@ export default authkitMiddleware();
 
 // Match against pages that require authentication
 // Leave this out if you want authentication on every page in your application
-export const config = { matcher: ['/'] };
+export const config = {
+  matcher: [
+    '/',
+    '/new-listing',
+    '/new-listing/:orgId*',
+    '/new-company',
+    '/jobs/:orgId*',
+    '/jobs/edit/:jobId*',
+    '/show/:jobId*',
+  ]
+};
