c1dbb9b9217fb2c8900366bf6db73cf011de454c
Raj Patel <patelraj24365@gmail.com>
2024-06-27 13:47:14 -0400

    authentication

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,18 +1,48 @@
-import Link from 'next/link'
-import React from 'react'
+import {
+  getSignInUrl,
+  getSignUpUrl,
+  getUser,
+  signOut,
+} from "@workos-inc/authkit-nextjs";
+import Link from "next/link";
+import React from "react";
 
-const Header = () => {
+const Header = async () => {
+  const { user } = await getUser();
+  const signInUrl = await getSignInUrl();
+  const signOutUrl = await getSignUpUrl();
+
   return (
     <header>
-          <div className="container flex items-center justify-between mx-auto my-4">
-          <Link href={"/"} className="font-bold text-xl">Job Board</Link>
-          <nav className="flex gap-4 *:py-2 *:px-2 *:rounded-md">
-          <Link className="bg-gray-300" href={"/login"}>Login</Link>
-          <Link className="bg-blue-600 text-white"href={"/new-listing"}>Post a job</Link>
+      <div className="container flex items-center justify-between mx-auto my-4">
+        <Link href={"/"} className="font-bold text-xl">
+          Job Board
+        </Link>
+        <nav className="flex gap-4">
+          {!user && (
+            <Link className="bg-gray-300 rounded-md px-2 py-2" href={signInUrl}>
+              Login
+            </Link>
+          )}
+          {user && (
+            <form
+              action={async () => {
+                "use server";
+                await signOut();
+              }}
+            >
+              <button type="submit" className="bg-gray-300 py-2 px-2 rounded-md">
+                Logout, {user.firstName}
+              </button>
+            </form>
+          )}
+          <Link className="bg-blue-600 text-white px-2 py-2 rounded-md" href={"/new-listing"}>
+            Post a job
+          </Link>
         </nav>
-          </div>       
-        </header>
-  )
-}
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
